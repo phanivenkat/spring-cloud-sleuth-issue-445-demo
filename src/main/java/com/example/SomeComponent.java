53e3c18b48b9813926503adf94970397a03727c5
Thomas Weidner <thomas.weidner@it-factum.de>
2016-11-08 14:20:31 +0100

    initial commit

---
+++ src/main/java/com/example/SomeComponent.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.concurrent.ExecutorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by weidner on 08/11/16.
+ */
+@Controller
+public class SomeComponent {
+
+    @Autowired
+    ExecutorService service;
+
+}
---
+++ src/main/java/com/example/SpringCloudSleuthIssue445DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringCloudSleuthIssue445DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSleuthIssue445DemoApplication.class, args);
+	}
+
+	@Bean
+	ExecutorService executorService() {
+		return Executors.newSingleThreadExecutor();
+	}
+}
